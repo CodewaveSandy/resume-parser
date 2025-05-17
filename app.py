e8425238ee5bd2fc341dbc7e611b04bd17b81540
SandipDhang <sandip.dhang@yahoo.com>
2025-05-17 21:04:39 +0530

    Updating packages and creating a basic script

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ load_dotenv()  # ✅ Load variables from .env into os.environ
 
 app = Flask(__name__)
 API_KEY = os.environ.get("API_KEY")
+port = int(os.environ.get("PORT", 3002)) 
 
 @app.before_request
 def check_api_key():
@@ -18,4 +19,5 @@ def hello():
     return jsonify({"message": "Hello from Python"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
+
